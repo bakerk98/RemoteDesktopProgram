876dad8a64afaec70615d7b44f91a3351b87a5e1
bakerk98 <56518784+bakerk98@users.noreply.github.com>
2021-06-17 12:32:01 -0400

    uploaded file
    first commit

---
+++ remote_desktop.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <gtk/gtk.h>
+#include <stdlib.h>
+
+/* this program makes a window that should be used with Steam */
+/* Remote Desktop to use the desktop of a host computer while afk from that host computer */
+
+/* this is my first venture into gtk, please excuse any poor craftsmenship, but note i had fun making this */
+
+static GtkWidget *close_window_please;
+static GtkWidget *minimize;
+static GtkWidget *window;
+
+void minimize_the_window_mommy(GtkWidget *minimize, gpointer data){
+  gtk_window_iconify(window);
+}
+
+int main(int argc, char **argv){
+  GtkWidget *grid;
+  gtk_init(&argc, &argv);
+  
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+  grid = gtk_grid_new();
+  gtk_container_add(GTK_CONTAINER(window), grid);
+
+  close_window_please=gtk_button_new_with_label("Exit");
+  g_signal_connect(close_window_please, "clicked", G_CALLBACK(gtk_main_quit), NULL);
+  gtk_grid_attach(GTK_GRID(grid), close_window_please, 0,0,1,1);
+
+  minimize=gtk_button_new_with_label("Minimize this window and begin Remote Desktopping...");
+  g_signal_connect(minimize, "clicked", G_CALLBACK(minimize_the_window_mommy), NULL);
+  gtk_grid_attach(GTK_GRID(grid), minimize, 1,0,1,1);
+
+  gtk_widget_show_all(window);
+  gtk_main();
+
+  return 0;
+}
